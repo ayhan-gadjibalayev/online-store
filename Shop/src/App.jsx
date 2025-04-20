74c6b23da52d9ea1d2e91df4fe3a02f19e2bfa2d
Ayhan <ayhan.gadjibalayev@gmail.com>
2025-04-20 13:36:08 +0300

    first commit

---
+++ Shop/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="general-div">
+      <div className="all-categories"></div>
+      <div className="right-block">
+        <div className="sorts"></div>
+        <div className="best-selling"></div>
+        <div className="most-popular"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
