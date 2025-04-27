087aff9c2e92fc3ad5f46e0131926803b8bebb0e
Ayhan <ayhan.gadjibalayev@gmail.com>
2025-04-27 12:03:42 +0300

    сделал дополнительные правки

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./component/AllCategories/Allcategories.css"
 import Team from "./component/AllCategories/Team.jsx"
 import Personal from "./component/AllCategories/Personal.jsx"
 import FoodDrink from "./component/AllCategories/FoodDrink.jsx"
